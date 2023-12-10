702d0fe57abbf863734c0497379eed97061e4ef4
marcbudd <marcbuddemeier@gmail.com>
2023-12-10 23:03:40 +0100

    Add imprint controller

---
+++ internal/controllers/imprint_controller_test.go
@@ -0,0 +1,33 @@
+package controllers
+
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+// TestGetImprint tests the GetImprint function
+func TestGetImprint(t *testing.T) {
+	// Set up Gin
+	router := gin.Default()
+	router.GET("/imprint", GetImprint)
+
+	// Create a response recorder
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/imprint", nil)
+	router.ServeHTTP(w, req)
+
+	// Parse the JSON response
+	var response map[string]interface{}
+	err := json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Fatalf("Failed to unmarshal response: %v", err)
+	}
+
+	// Check if the 'text' key exists
+	if _, exists := response["text"]; !exists {
+		t.Fatalf("Response does not contain 'text' key")
+	}
+}
