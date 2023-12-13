c60f74815a2fcdaf470b5c1821b40bb61dfd420b
marcbudd <marcbuddemeier@gmail.com>
2023-12-13 16:38:54 +0100

    Implement user verification and login

--- internal/models/verification_code.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "time"
-
-type VerificationCode struct {
-	Username       string    `gorm:"primary_key"`
-	Code           string    `gorm:"primary_key;column:code;not_null"`
-	ExpirationTime time.Time `gorm:"column:expiration_time;not_null"`
-}
