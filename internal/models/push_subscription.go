32cb63963617ad5d52861c563303b2fb170833a4
marcbudd <marcbuddemeier@gmail.com>
2024-05-22 20:56:50 +0200

    Add push notifications using expo

--- internal/models/push_subscription.go
+++ internal/models/push_subscription.go
@@ -7,9 +7,10 @@ type PushSubscription struct {
 	Username string    `gorm:"column:username_fk;type:varchar(20)"`
 	User     User      `gorm:"foreignKey:username_fk;references:username"`
 	Type     string    `gorm:"column:type;type:varchar(4)"` // either "web" or "expo"
-	Endpoint string    `gorm:"column:endpoint;type:text"`
-	P256dh   string    `gorm:"column:p256dh;type:text"`
-	Auth     string    `gorm:"column:auth;type:text"`
+	Endpoint string    `gorm:"column:endpoint;type:text"`   // for web only
+	P256dh   string    `gorm:"column:p256dh;type:text"`     // for web only
+	Auth     string    `gorm:"column:auth;type:text"`       // for web only
+	Token    string    `gorm:"column:token;type:text"`      // for expo only
 }
 
 type VapidKeyResponseDTO struct {
@@ -17,18 +18,19 @@ type VapidKeyResponseDTO struct {
 }
 
 type SubscriptionKeys struct {
-	P256dh string `json:"p256dh"`
-	Auth   string `json:"auth"`
+	P256dh string `json:"p256dh" binding:"required"`
+	Auth   string `json:"auth" binding:"required"`
 }
 
 type SubscriptionInfo struct {
-	Endpoint         string           `json:"endpoint"`
-	SubscriptionKeys SubscriptionKeys `json:"keys"`
+	Endpoint         string           `json:"endpoint" binding:"required"`
+	SubscriptionKeys SubscriptionKeys `json:"keys" binding:"required"`
 }
 
 type PushSubscriptionRequestDTO struct {
 	Type             string           `json:"type" binding:"required"`
-	SubscriptionInfo SubscriptionInfo `json:"subscription" binding:"required"`
+	SubscriptionInfo SubscriptionInfo `json:"subscription"` // subscription info for web push notifications
+	Token            string           `json:"token"`        // token for expo push notifications
 }
 
 type PushSubscriptionResponseDTO struct {
