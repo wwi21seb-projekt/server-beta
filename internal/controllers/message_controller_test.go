84743b797424a35adb3c0756f541f0e4a639bfa2
marcbudd <marcbuddemeier@gmail.com>
2024-06-06 19:45:00 +0200

    Fix HandleWebsocketSuccess test

--- internal/controllers/message_controller_test.go
+++ internal/controllers/message_controller_test.go
@@ -271,6 +271,9 @@ func TestHandleWebSocketSuccess(t *testing.T) {
 	server := httptest.NewServer(router)
 	defer server.Close()
 
+	// Wait for server to start
+	time.Sleep(1 * time.Second)
+
 	// Create WebSocket connection
 	url := "ws" + server.URL[4:] + "/chat?chatId=" + chat.Id.String()
 	headers := http.Header{"Sec-WebSocket-Protocol": []string{authenticationToken}}
@@ -291,6 +294,9 @@ func TestHandleWebSocketSuccess(t *testing.T) {
 	}(ws2)
 	_ = ws2.SetReadDeadline(time.Now().Add(10 * time.Second)) // set read deadline to avoid blocking
 
+	// Wait for connections to establish
+	time.Sleep(1 * time.Second)
+
 	// Send message from current user
 	message := models.MessageCreateRequestDTO{
 		Content: "Test message",
@@ -300,6 +306,9 @@ func TestHandleWebSocketSuccess(t *testing.T) {
 
 	err = ws.WriteMessage(websocket.TextMessage, messageJSON)
 	assert.NoError(t, err)
+
+	//Wait for message to send
+	time.Sleep(1 * time.Second)
 
 	// Read message from current user
 	_, receivedMessage, err := ws.ReadMessage()
@@ -346,6 +355,162 @@ func TestHandleWebSocketSuccess(t *testing.T) {
 	mockChatRepository.AssertExpectations(t)
 	mockPushSubscriptionRepository.AssertExpectations(t)
 	mockNotificationRepository.AssertExpectations(t)
+}
+
+// TestHandleWebSocketSuccess tests if the HandleWebSocket establishes a connection and is able to send and receive messages
+func TestHandleWebSocketSuccesst(t *testing.T) {
+	// Arrange
+	mockChatRepository := new(repositories.MockChatRepository)
+	mockMessageRepository := new(repositories.MockMessageRepository)
+	mockNotificationRepository := new(repositories.MockNotificationRepository)
+	mockPushSubscriptionRepository := new(repositories.MockPushSubscriptionRepository)
+	pushSubscriptionService := services.NewPushSubscriptionService(mockPushSubscriptionRepository)
+	notificationService := services.NewNotificationService(mockNotificationRepository, pushSubscriptionService)
+	messageService := services.NewMessageService(mockMessageRepository, mockChatRepository, notificationService)
+	messageController := controllers.NewMessageController(messageService)
+
+	currentUsername := "myUser"
+	authenticationToken, err := utils.GenerateAccessToken(currentUsername)
+	if err != nil {
+		t.Fatal(err)
+	}
+	otherUsername := "otherUser"
+	secondOtherUsername := "secondOtherUser"
+	authTokenSecondOther, err := utils.GenerateAccessToken(secondOtherUsername)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	chat := models.Chat{
+		Id: uuid.New(),
+		Users: []models.User{
+			{Username: currentUsername},
+			{Username: otherUsername},
+			{Username: secondOtherUsername},
+		},
+	}
+
+	// Mock expectations
+	var capturedMessage *models.Message
+	var capturedNotification *models.Notification
+	mockChatRepository.On("GetChatById", chat.Id.String()).Return(chat, nil)
+	mockMessageRepository.On("CreateMessage", mock.AnythingOfType("*models.Message")).
+		Run(func(args mock.Arguments) {
+			capturedMessage = args.Get(0).(*models.Message)
+		}).Return(nil)
+
+	// First other user has no open connection --> expect notification
+	mockNotificationRepository.On("CreateNotification", mock.AnythingOfType("*models.Notification")).
+		Run(func(args mock.Arguments) {
+			capturedNotification = args.Get(0).(*models.Notification)
+		}).Return(nil)
+	mockPushSubscriptionRepository.On("GetPushSubscriptionsByUsername", otherUsername).Return([]models.PushSubscription{}, nil)
+
+	// Create test server
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	router.GET("/chat", messageController.HandleWebSocket)
+	server := httptest.NewServer(router)
+	defer server.Close()
+
+	// Create WebSocket connection
+	url := "ws" + server.URL[4:] + "/chat?chatId=" + chat.Id.String()
+	headers := http.Header{"Sec-WebSocket-Protocol": []string{authenticationToken}}
+	ws, _, err := websocket.DefaultDialer.Dial(url, headers)
+	assert.NoError(t, err)
+	defer func(ws *websocket.Conn) {
+		_ = ws.Close()
+	}(ws)
+	_ = ws.SetReadDeadline(time.Now().Add(30 * time.Second)) // set read deadline to avoid blocking
+
+	// Create WebSocket connection for second other user
+	url = "ws" + server.URL[4:] + "/chat?chatId=" + chat.Id.String()
+	headers = http.Header{"Sec-WebSocket-Protocol": []string{authTokenSecondOther}}
+	ws2, _, err := websocket.DefaultDialer.Dial(url, headers)
+	assert.NoError(t, err)
+	defer func(ws2 *websocket.Conn) {
+		_ = ws2.Close()
+	}(ws2)
+	_ = ws2.SetReadDeadline(time.Now().Add(30 * time.Second)) // set read deadline to avoid blocking
+
+	// Send message from current user
+	message := models.MessageCreateRequestDTO{
+		Content: "Test message",
+	}
+	messageJSON, err := json.Marshal(message)
+	assert.NoError(t, err)
+
+	err = ws.WriteMessage(websocket.TextMessage, messageJSON)
+	assert.NoError(t, err)
+
+	// Read message from current user
+	_, receivedMessage, err := ws.ReadMessage()
+	if err != nil {
+		t.Logf("Error reading message from current user: %v", err)
+	}
+	assert.NoError(t, err)
+	var response models.MessageRecordDTO
+	err = json.Unmarshal(receivedMessage, &response)
+	if err != nil {
+		t.Logf("Error unmarshalling message from current user: %v", err)
+	}
+	assert.NoError(t, err)
+
+	// Read message from second other user
+	_, receivedMessage2, err := ws2.ReadMessage()
+	if err != nil {
+		t.Logf("Error reading message from second other user: %v", err)
+	}
+	assert.NoError(t, err)
+	var response2 models.MessageRecordDTO
+	err = json.Unmarshal(receivedMessage2, &response2)
+	if err != nil {
+		t.Logf("Error unmarshalling message from second other user: %v", err)
+	}
+	assert.NoError(t, err)
+
+	// Assertions
+	// Current user response
+	assert.Equal(t, message.Content, response.Content)
+	assert.Equal(t, currentUsername, response.Username)
+	assert.NotEmpty(t, response.CreationDate)
+
+	// Second other user response
+	assert.Equal(t, message.Content, response2.Content)
+	assert.Equal(t, currentUsername, response2.Username)
+	assert.NotEmpty(t, response2.CreationDate)
+
+	// Captured message
+	assert.NotNil(t, capturedMessage)
+	assert.Equal(t, message.Content, capturedMessage.Content)
+	assert.Equal(t, currentUsername, capturedMessage.Username)
+	assert.Equal(t, chat.Id, capturedMessage.ChatId)
+	assert.NotEmpty(t, capturedMessage.Id)
+	assert.True(t, capturedMessage.CreatedAt.Equal(response.CreationDate))
+
+	// Captured notification
+	assert.NotNil(t, capturedNotification)
+	assert.Equal(t, "message", capturedNotification.NotificationType)
+	assert.Equal(t, otherUsername, capturedNotification.ForUsername)
+	assert.Equal(t, currentUsername, capturedNotification.FromUsername)
+	assert.NotEmpty(t, capturedNotification.Id)
+	assert.NotEmpty(t, capturedNotification.Timestamp)
+
+	mockMessageRepository.AssertExpectations(t)
+	mockChatRepository.AssertExpectations(t)
+	mockPushSubscriptionRepository.AssertExpectations(t)
+	mockNotificationRepository.AssertExpectations(t)
+
+	// Check for normal closure on both WebSocket connections
+	//ws.Close() // Explicitly close the WebSocket connection
+	//_, _, err = ws.ReadMessage()
+	//assert.Error(t, err)
+	//assert.Equal(t, websocket.IsCloseError(err, websocket.CloseNormalClosure), true)
+	//
+	//ws2.Close() // Explicitly close the second WebSocket connection
+	//_, _, err = ws2.ReadMessage()
+	//assert.Error(t, err)
+	//assert.Equal(t, websocket.IsCloseError(err, websocket.CloseNormalClosure), true)
 }
 
 // TestHandleWebSocketBadRequest tests if the HandleWebSocket function returns BadRequest custom error when the message is invalid
